7577bad62e716614d0f6a7846e5335a17a87a507
mattjkatz <matthewkatz7@gmail.com>
2022-06-24 13:21:18 -0500

    add a footer

---
+++ src/Footer.js
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <h5>I should be wearing shoes because I am a footer</h5>;
+}
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./style.css";
 import Navbar from "./Navbar.js";
 import App from "./App.js";
+import Footer from "./Footer.js";
 
 function Everything() {
   return (
     <div>
       <Navbar />
       <App />
+      <Footer />
     </div>
   );
 }
