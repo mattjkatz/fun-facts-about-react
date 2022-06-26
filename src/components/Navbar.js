ee6189b11d8427bee1b7209bc8a5f98ef1af2e41
mattjkatz <matthewkatz7@gmail.com>
2022-06-26 15:55:52 -0500

    begin working on navbar css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,8 @@
 export default function Navbar() {
-  return <h1>GO!</h1>;
+  return (
+    <div class="navbar">
+      <h1 class="navbar-title">NavBar</h1>
+      <h1 class="navbar-description">React Course - Project 1</h1>
+    </div>
+  );
 }
