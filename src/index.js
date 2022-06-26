117e07f7c7a0914dbf7b3021a8e3fda1aeea6d2a
mattjkatz <matthewkatz7@gmail.com>
2022-06-26 15:30:40 -0500

    all set up

--- src/index.js
+++ src/index.js
@@ -3,13 +3,5 @@ import ReactDOM from "react-dom/client";
 import "./style.css";
 import App from "./App.js";
 
-function Index() {
-  return (
-    <div>
-      <App />
-    </div>
-  );
-}
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Index />);
+root.render(<App />);
