60b1b5315cb2c7c637bf2c3aa013d3f8a23373d8
mattjkatz <matthewkatz7@gmail.com>
2022-06-27 18:50:05 -0500

    columns or flex hmmmm

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 export default function Navbar() {
   return (
     <div class="navbar">
-      <h1 class="navbar-title">NavBar</h1>
-      <h1 class="navbar-description">React Course - Project 1</h1>
+      <h1 class="navbar-title col">NavBar</h1>
+      <h1 class="navbar-description col">React Course - Project 1</h1>
     </div>
   );
 }
