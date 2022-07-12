93d2ad51e803bf0ce2cf0342673a74364b9bf723
mattjkatz <matthewkatz7@gmail.com>
2022-07-12 11:22:34 -0500

    new image and broken css logo class

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
+import "./../style.css";
+
 export default function Navbar() {
   return (
     <nav>
-      <img src={require("../images/react-icon-small.png")} alt="" />
+      <img class="header-logo" src={require("../images/react-icon-small.png")} alt="" />
       <h3>ReactFacts</h3>
       <h4>React Course - Project 1</h4>
     </nav>
