afed194b6662931a3c48b1812b3dbd278889ebfa
mattjkatz <matthewkatz7@gmail.com>
2022-07-14 19:48:38 -0500

    styling done right - no longer a silly goose

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
 export default function Navbar() {
   return (
     <nav>
-      <img src={require("../images/react-icon-small.png")} alt="" />
-      <h3>ReactFacts</h3>
-      <h4>React Course - Project 1</h4>
+      <img src={require("../images/react-icon-small.png")} alt="" className="nav--icon" />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
     </nav>
   );
 }
